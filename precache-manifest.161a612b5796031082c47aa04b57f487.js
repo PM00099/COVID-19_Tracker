7a2f3266dd40e5788669a7171b83d6faa13dee0b
Priyank Mithaiwala <priyankmithaiwala@gmail.com>
2020-04-28 11:13:12 +0530

    Initial commit

---
+++ precache-manifest.161a612b5796031082c47aa04b57f487.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "379d17e7ecc060cc8c42140783381fd7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c28e488037765e7b14bd",
+    "url": "/static/css/main.26b5568a.chunk.css"
+  },
+  {
+    "revision": "b9dc13e1f2d6b9198de0",
+    "url": "/static/js/2.81f14f01.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/static/js/2.81f14f01.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c28e488037765e7b14bd",
+    "url": "/static/js/main.c6d14d25.chunk.js"
+  },
+  {
+    "revision": "690f225c668be9ff41d9",
+    "url": "/static/js/runtime-main.af7f4d6c.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/static/media/image.d7265326.png"
+  }
+]);
